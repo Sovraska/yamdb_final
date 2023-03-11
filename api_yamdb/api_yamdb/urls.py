abf6a3d4f84a73d31c7fe20d73d02bd80dee3248
Sovraska <simon.novikov@gmail.com>
2023-03-11 21:09:51 +0300

    redock try 8 PEP8 2

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -20,6 +20,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0
+    ), name='schema-redoc'),
 
 ]
